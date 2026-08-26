2637b488e800fef414e3dd98f19517d1ee93e6d2
DennisL68 <dennis.lindqvist68@outlook.com>
2026-08-26 11:47:11 +0200

    test: Added mock unit tests

---
+++ tests/Unit/Public/Show-OrionDemo.tests.ps1
@@ -0,0 +1,27 @@
+﻿BeforeAll {
+    $script:dscModuleName = 'OrionDesign'
+
+    Import-Module -Name $script:dscModuleName
+}
+
+AfterAll {
+    # Unload the module being tested so that it doesn't impact any other tests.
+    Get-Module -Name $script:dscModuleName -All | Remove-Module -Force
+}
+
+Describe Get-Something {
+    BeforeAll {
+        Mock -CommandName Get-PrivateFunction -MockWith {
+            # This return the value passed to the Get-PrivateFunction parameter $PrivateData.
+            $PrivateData
+        } -ModuleName $dscModuleName
+    }
+
+    Context 'When tested, skip for now' {
+        It 'Should skip the test' -Skip {
+            #No test yet
+        }
+    }
+
+}
+
